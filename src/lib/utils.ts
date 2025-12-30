e13c99cc07528dcfb6b1eed0f2d65e0404367fec
Alexandre Servies <alex.servies@gmail.com>
2025-12-30 13:11:45 -0800

    ee

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -2,12 +2,6 @@ import {ChartDataPoint, HourlyDataPoint, UsageResponse} from "./types";
 import {DateRange} from "./api";
 
 export function formatNumber(num: number): string {
-  if (num >= 1000000) {
-    return (num / 1000000).toFixed(1) + "M";
-  }
-  if (num >= 1000) {
-    return (num / 1000).toFixed(1) + "K";
-  }
   return num.toLocaleString();
 }
 
